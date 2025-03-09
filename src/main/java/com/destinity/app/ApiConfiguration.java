27c28cedd961b0401091fc11e960d46492c0c52e
Eddys912 <eddys912sc@gmail.com>
2025-03-09 02:03:04 -0600

    Add API configuration application path

---
+++ src/main/java/com/destinity/app/ApiConfiguration.java
@@ -0,0 +1,8 @@
+package com.destinity.app;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("erp/api")
+public class ApiConfiguration extends Application {
+}
